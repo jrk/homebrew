d499330ec95119a11df1055860a0c07c49b051fc
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-05-20 20:23:28 -0400

    Added patches to use ksh for GODI bootstrap script

--- Library/Formula/godi.rb
+++ Library/Formula/godi.rb
@@ -6,8 +6,24 @@ class Godi < Formula
   md5 '5ce87273e3ecce6a3785a6c538674e1e'
   version '3.12'
 
+  def patches
+    # set bootstrap script to use /bin/ksh instead of /bin/sh
+    DATA
+  end
+
   def install
     system "./bootstrap", "--prefix", "#{prefix}", "--section", "3.12"
     system "PATH=#{prefix}/bin:#{prefix}/sbin:$PATH ./bootstrap_stage2"
   end
 end
+
+__END__
+diff --git a/bootstrap b/bootstrap
+index 258f815..ffe7dab 100755
+--- a/bootstrap
++++ b/bootstrap
+@@ -1,4 +1,4 @@
+-#! /bin/sh
++#! /bin/ksh
+ 
+ # GODI: $Id: bootstrap 912 2009-12-22 15:50:49Z gerd $
